77b96562b712b9aa4fb40c248a9b52631e5cd220
Sacules <leociancalucas@gmail.com>
2018-12-31 18:34:02 -0300

    Implemented Add, Replace, ShowCurrent methods.

--- queue.go
+++ queue.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Queue is the overall scheduler, consisting of 4 blocks.
 type Queue [4]*Block
 
@@ -9,12 +13,26 @@ type Queue [4]*Block
 
 // Add a block to the queue and get rid of any old ones.
 func (q *Queue) Add(b *Block) {
+	for i := len(q); i > 0; i-- {
+		q[i] = q[i-1]
+	}
+
+	q[0] = b
 }
 
 // Replace goes through the queue and replaces an album for a new one.
 func (q *Queue) Replace(old, actual Album) {
+	for _, block := range q {
+		block.ReplaceAlbum(old, actual)
+	}
 }
 
 // ShowCurrent prints the current week of records in the queue.
 func (q *Queue) ShowCurrent() {
+	for _, block := range q {
+		fmt.Printf("\n%s", block.Name)
+		for i, album := range block.Albums {
+			fmt.Printf("%d. %s", i+1, album)
+		}
+	}
 }
