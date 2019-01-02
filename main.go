a5720f425ef7f2b30bc72068939a2813a5df164c
Sacules <leociancalucas@gmail.com>
2019-01-02 17:15:27 -0300

    Added status flag.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 )
 
 var (
@@ -12,8 +13,22 @@ var (
 func main() {
 	flag.BoolVar(&verbose, "v", false, "verbose mode")
 	flag.Parse()
+	if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "status":
+		q := &Queue{}
+		q.Load()
+		q.ShowCurrent()
+
+	default:
+		flag.PrintDefaults()
+	}
 
 	// Testing
-	api := InitAPI()
-	LookupArtwork(api, "dark side of the moon", "cache/week 2", verbose)
+	// api := InitAPI()
+	// LookupArtwork(api, "dark side of the moon", "cache/week 2", verbose)
 }
