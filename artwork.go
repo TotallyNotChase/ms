0941dad4d01f91096767b68cba69822bf8bd0a65
Sacules <leociancalucas@gmail.com>
2019-01-02 15:14:34 -0300

    Slightly clearer checking for cached images.

--- artwork.go
+++ artwork.go
@@ -50,15 +50,15 @@ func GetArtwork(api *lastfm.Api, album string, dir string, results chan string, 
 	}
 
 	for _, match := range al.AlbumMatches {
+		// Don't download if it's already cached!
+		filename := strings.Replace(match.Name, "/", " ", -1)
+		if _, err := os.Stat(dir + "/" + filename + ".png"); !os.IsNotExist(err) {
+			continue
+		}
+
 		for _, image := range match.Images {
 			// Only work with big images
 			if image.Size == "large" && image.Url != "" {
-				filename := strings.Replace(match.Name, "/", " ", -1)
-				// Don't download if it's already cached!
-				if _, err := os.Stat(dir + "/" + filename + ".png"); !os.IsNotExist(err) {
-					break
-				}
-
 				data := downData{
 					url:  image.Url,
 					name: filename,
