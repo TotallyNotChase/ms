7e70b0446892831e675301df0133400e8fca5141
Sacules <leociancalucas@gmail.com>
2019-01-01 21:21:51 -0300

    Added provisory main.

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/shkh/lastfm-go/lastfm"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	apiKey := &APIKey{}
+	err := apiKey.Load()
+	if err != nil {
+		fmt.Println("API Key couldn't be found, creating a new apikey.json file.")
+		fmt.Println("Please put your keys there before using this program.")
+		apiKey.Save()
+		os.Exit(1)
+	}
+
+	api := lastfm.New(apiKey.Key, apiKey.Secret)
+
+	// Testing
+	err = GetArtwork(api, "Michael Jackson", "Thriller", "nice")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
