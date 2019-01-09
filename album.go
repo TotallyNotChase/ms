242bd17beb46e83208431a3479e8050bc3c36c66
Sacules <leociancalucas@gmail.com>
2019-01-09 19:05:41 -0300

    Added JSON tags to Album struct.

--- album.go
+++ album.go
@@ -3,7 +3,7 @@ package main
 // Album is a record to be scheduled. Includes whether it's been listened
 // or rated.
 type Album struct {
-	Name     string
-	Listened bool
-	Rated    bool
+	Name     string `json:"name"`
+	Listened bool   `json:"listened"`
+	Rated    bool   `json:"rated"`
 }
