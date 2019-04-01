0c68d1136622bbf528715b8b7e269aaba3295e58
Lucas Leonardo <lucasleonardo@protonmail.com>
2019-04-01 01:18:46 -0300

    Added back option to create new block.

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
+import (
+	"os"
+)
+
 var (
 	// Flags
 	verbose bool
 )
 
 func main() {
+	if len(os.Args) > 1 {
+		if os.Args[1] == "newblock" {
+			newblock()
+		}
+
+		return
+	}
+
 	tui := newTui()
 
 	tui.init()
