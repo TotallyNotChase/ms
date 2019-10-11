eca4770c7f724372162a612ae868749dd70ebb2b
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2019-10-11 13:47:19 +0530

    Add create/remove ms.lock functionality to main
    This should handle both signal exits and normal exits, however it will NOT handle `os.exit()`

--- main.go
+++ main.go
@@ -2,9 +2,41 @@ package main
 
 import (
 	"os"
+  "os/signal"
 )
 
 func main() {
+	var test string
+	exitsig := make(chan os.Signal, 1)
+	signal.Notify(exitsig, os.Interrupt)
+	var (
+			lockstate bool = false
+	)
+
+	go func() {
+			<-exitsig
+			if lockstate {
+					var err = os.Remove("ms.lock")
+					if err != nil {
+							return
+					}
+			}
+			os.Exit(0)
+	}()
+
+	if _, err := os.Stat("ms.lock"); err == nil {
+			return
+
+	} else if os.IsNotExist(err) {
+			var file, err = os.Create("ms.lock")
+			if err != nil {
+					return
+			}
+			file.Close()
+			defer os.Remove("ms.lock")
+			lockstate = true
+	}
+	
 	if len(os.Args) > 1 {
 		if os.Args[1] == "newblock" {
 			newblock()
