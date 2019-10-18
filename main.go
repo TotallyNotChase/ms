c0aa615e180a788b996e23af55173bf1eb85e385
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2019-10-18 09:32:59 +0530

    Update main.go accordingly

--- main.go
+++ main.go
@@ -2,39 +2,39 @@ package main
 
 import (
 	"os"
-  "os/signal"
+  	"os/signal"
 )
 
 func main() {
-	var test string
 	exitsig := make(chan os.Signal, 1)
 	signal.Notify(exitsig, os.Interrupt)
-	var (
-			lockstate bool = false
-	)
+	lockstate := false
 
 	go func() {
-			<-exitsig
-			if lockstate {
-					var err = os.Remove("ms.lock")
-					if err != nil {
-							return
-					}
+		<-exitsig
+		if lockstate {
+			err := os.Remove("ms.lock")
+			if err != nil {
+				fmt.Printf("couldn't remove the lock: %v", err)
+				return
 			}
-			os.Exit(0)
+		}
+		os.Exit(0)
 	}()
 
 	if _, err := os.Stat("ms.lock"); err == nil {
-			return
+		fmt.Printf("Cannot run program : Another Instance already running")
+		return
 
 	} else if os.IsNotExist(err) {
-			var file, err = os.Create("ms.lock")
-			if err != nil {
-					return
-			}
-			file.Close()
-			defer os.Remove("ms.lock")
-			lockstate = true
+		var file, err = os.Create("ms.lock")
+		if err != nil {
+			fmt.Printf("couldn't create the lock: %v", err)
+			return
+		}
+		file.Close()
+		defer os.Remove("ms.lock")
+		lockstate = true
 	}
 	
 	if len(os.Args) > 1 {
